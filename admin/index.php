e34a10d956f7c1d45bba47105c9b6e429adc071d
Darkzidious <nana_zensei@hotmail.com>
2018-10-29 00:55:11 +0700

    แก้ หน้า login กับ หน้าแรก

--- admin/index.php
+++ admin/index.php
@@ -28,13 +28,74 @@ if(isset($_POST['submit']) && $_POST['submit'] == "Login"){
 
 ?>
 
+<!DOCTYPE html>
+<html>
 
-<form method="POST" action="">
+<head>
+    <title>บึงบัว</title>
+    <meta charset="utf-8">
+    <meta name="viewport">
+
+    <!-- Bootstrap -->
+    <link href="../admin/assets/option/vendors//bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom Theme Style -->
+    <link href="../admin/assets/option/build/css/custom.min.css" rel="stylesheet">
+
+    <!-- jQuery -->
+    <script src="../admin/assets/option/vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="../admin/assets/option/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- edit css by tasto -->
+    <link href="../admin/assets/css/login.css" rel="stylesheet">
+</head>
+
+<body class="backgroud body">
+    <div class="picture">
+        <center>
+            <img src="../admin/assets/images/login.png" style="width:320px;margin-bottom: 2%;margin-top: 5%; ">
+            <p style="font-size:150%">ADMIN ชุมชนบึงบัว</p>
+        </center>
+    </div>
+    <form class="" method="POST" action="">
+            <?php 
+                $helper->showMsg(); //เพิ่ม pop up login ผิดด้วย
+            ?>
+        <div class="form-group">
+            <label class="control-label col-md-5 col-sm-5 col-xs-12" for="username" style="text-align:right; padding:8px;font-size: 120%;">
+                USERNAME :
+            </label>
+            <div class="col-md-3 col-sm-3 col-xs-12">
+                <input type="text" name="username" class="form-control" required="" autofocus="" style="border-radius:10px;width: 85%">
+            </div>
+        </div>
+        <br><br><br>
+        <div class="form-group">
+            <label class="control-label col-md-5 col-sm-5 col-xs-12" for="password" style="text-align:right; padding:8px;font-size: 120%;">
+                PASSWORD :
+            </label>
+            <div class="col-md-3 col-sm-3 col-xs-12">
+                <input type="password" name="password" class="form-control" required="" autofocus="" style="border-radius:10px;width: 85%">
+            </div>
+        </div>
+        <br><br>
+        <center>
+            <a href="home.html">
+                <button class=" btn-lg btn-success btn-block" type="submit" name="submit" value="Login" style="margin-top:10px;width: 12%">LOGIN</button>
+            </a>
+        </center>
+    </form>
+</body>
+
+</html>
+
+
+<!-- <form method="POST" action="">
     <h3>Login</h3>
 
-    <?php 
-        $helper->showMsg();
-    ?>
+    // <?php 
+    //     $helper->showMsg();
+    // ?>
 
     Username : <input type="text" name="username">
     <br>
@@ -42,4 +103,4 @@ if(isset($_POST['submit']) && $_POST['submit'] == "Login"){
     <br>
     <input type="submit" name="submit" value="Login">
 
-</form>
+</form> -->
